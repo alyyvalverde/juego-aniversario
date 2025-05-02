acb71720a9781a2b96a81ac093b24fdbc9307927
Aly Valverde <allie@Alys-MacBook-Air.local>
2025-05-02 15:46:30 -0600

    Agregando imágenes y juego completo

--- main.js
+++
@@ -1,6 +0,0 @@
-function preload() {
-    this.load.image('corazon', 'assets/corazon.png');
-    this.load.image('ground', 'assets/ground.png');
-    this.load.image('fondo', 'assets/fondo.png');
-    this.load.spritesheet( { frameWidth: 32, frameHeight: 48 });
-  }  
